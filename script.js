b89f234cc9d6a7fb48b4cfc843dc4540c9d10ed6
VFD9654 <117727274+ianchzhu@users.noreply.github.com>
2026-01-29 07:28:12 +0000

    Added path showing

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ const file_icon = {
 }
 // 获取 URL 中的 path 参数，并确保格式统一（以 / 结尾）
 const currentPath = (new URLSearchParams(document.location.search)).get("path") || "";
+document.getElementById("path").innerText = "/"+currentPath;
 // --- Your Custom Dual SHA-512 Hashing Logic ---
 async function sha512(str) {
     const encoder = new TextEncoder();
